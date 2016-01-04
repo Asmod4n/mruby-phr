5d2a634b34f329d0d2e82d51d8fd8ee5bcda3ac9
Asmod4n <Asmod4n@users.noreply.github.com>
2016-01-04 13:57:14 +0100

    handle mrb arena, further style fixups

--- src/mrb_phr.c
+++ src/mrb_phr.c
@@ -14,6 +14,7 @@ static inline mrb_value
 mrb_phr_headers_to_a(mrb_state *mrb, mrb_value buff_obj, struct phr_header *headers, size_t num_headers)
 {
   mrb_value headers_array = mrb_ary_new_capa(mrb, num_headers);
+  int ai = mrb_gc_arena_save(mrb);
   for (size_t curr_header = 0;curr_header < num_headers; curr_header++) {
     mrb_value header_name = mrb_str_substr(mrb, buff_obj, headers[curr_header].name - RSTRING_PTR(buff_obj), headers[curr_header].name_len);
     mrb_funcall(mrb, header_name, "downcase!", 0);
@@ -21,6 +22,7 @@ mrb_phr_headers_to_a(mrb_state *mrb, mrb_value buff_obj, struct phr_header *head
     mrb_value name_value_pair = mrb_assoc_new(mrb, header_name, header_value);
 
     mrb_ary_push(mrb, headers_array, name_value_pair);
+    mrb_gc_arena_restore(mrb, ai);
   }
 
   return headers_array;
@@ -39,7 +41,7 @@ mrb_phr_parse_request(mrb_state *mrb, mrb_value self)
   size_t path_len;
   int minor_version;
   struct phr_header headers[PHR_MAX_HEADERS];
-  size_t num_headers = PHR_MAX_HEADERS;
+  size_t num_headers = sizeof(headers);
   mrb_int last_len = mrb_fixnum(mrb_iv_get(mrb, self,
     mrb_intern_lit(mrb, "last_len")));
 
@@ -74,11 +76,12 @@ mrb_phr_parse_response(mrb_state *mrb, mrb_value self)
   mrb_value response_obj;
 
   mrb_get_args(mrb, "S", &response_obj);
+
   int minor_version, status;
   const char *msg;
   size_t msg_len;
   struct phr_header headers[PHR_MAX_HEADERS];
-  size_t num_headers = PHR_MAX_HEADERS;
+  size_t num_headers = sizeof(headers);
   mrb_int last_len = mrb_fixnum(mrb_iv_get(mrb, self,
     mrb_intern_lit(mrb, "last_len")));
 
@@ -114,12 +117,12 @@ mrb_phr_parse_headers(mrb_state *mrb, mrb_value self)
   mrb_get_args(mrb, "S", &headers_obj);
 
   struct phr_header headers[PHR_MAX_HEADERS];
-  size_t num_headers = PHR_MAX_HEADERS;
+  size_t num_headers = sizeof(headers);
   mrb_int last_len = mrb_fixnum(mrb_iv_get(mrb, self,
     mrb_intern_lit(mrb, "last_len")));
 
   int pret = phr_parse_headers(RSTRING_PTR(headers_obj), RSTRING_LEN(headers_obj),
-    headers, &num_headers, (size_t) last_len);
+    headers, &num_headers, last_len);
 
   if (pret == -1) {
     return mrb_symbol_value(mrb_intern_lit(mrb, "parser_error"));
